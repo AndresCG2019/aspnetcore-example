5190e32d2bfab4d48da8754f7673f6e597545d3e
andres <andres96cst@outlook.com>
2024-04-25 16:06:10 -0600

    Se implementó SimpleEntityAlphabeticalOrderSpecification

---
+++ src/Empresa.Proyecto.Core/Specifications/SimpleEntityAlphabeticalOrderSpecification.cs
@@ -0,0 +1,25 @@
+﻿using Empresa.Proyecto.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa.Proyecto.Core.Specifications
+{
+    /// <summary>
+    /// Ejemplo clase de especificacion. Se crean las especificaciones necesarias para 
+    /// las diferentes consultas necesarias
+    /// </summary>
+    public class SimpleEntityAlphabeticalOrderSpecification : BaseSpecification<SimpleEntity>
+    {
+        /// <summary>
+        /// Esta especificacion recibe un parametro y aplica un filtro. 
+        /// Tambine se definen configuraciones auxiliares como Includes, orders y paginacion
+        /// </summary>
+        public SimpleEntityAlphabeticalOrderSpecification() 
+        {
+            ApplyOrderBy(c => c.Name);
+        }
+    }
+}
--- src/Empresa.Proyecto.Web/Pages/BaseCatalogPageModel.cs
+++ src/Empresa.Proyecto.Web/Pages/BaseCatalogPageModel.cs
@@ -2,17 +2,18 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Empresa.Proyecto.Core.Entities;
 using Empresa.Proyecto.Core.Interfaces;
+using Empresa.Proyecto.Core.Specifications;
 
 namespace Empresa.Proyecto.Web.Pages
 {
     public class BaseCatalogPageModel<T>: PageModel where T : BaseEntity
     {
-        private readonly IAsyncRepository<T> Repo;
+        private readonly IAsyncRepository<SimpleEntity> Repo;
         private readonly ILogger<BaseCatalogPageModel<T>> Logger;
 
         public IReadOnlyList<T> Entidades { get; set; } = null!;
 
-        public BaseCatalogPageModel(ILogger<BaseCatalogPageModel<T>> logger, IAsyncRepository<T> repo)
+        public BaseCatalogPageModel(ILogger<BaseCatalogPageModel<T>> logger, IAsyncRepository<SimpleEntity> repo)
         {
             Repo = repo;
             Logger = logger;
@@ -20,7 +21,9 @@ namespace Empresa.Proyecto.Web.Pages
 
         public async Task<JsonResult> OnPostCatalog()
         {
-            var catalog = await Repo.ListAllAsync();
+            var spec = new SimpleEntityAlphabeticalOrderSpecification();
+
+            var catalog = await Repo.ListAsync(spec);
             return new JsonResult(new { data = catalog });
         }
     }
