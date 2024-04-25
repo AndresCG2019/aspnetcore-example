3dcc24a9c3eedcf90b3733ba9f7c364aa9d63f59
andres <andres96cst@outlook.com>
2024-04-25 16:31:16 -0600

    Se agregó NewEntity

---
+++ src/Empresa.Proyecto.Core/Entities/NewEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Empresa.Proyecto.Core.Entities
+{
+    /// <summary>
+    /// Ejemplo de entidad compleja, como una entidad principal en dominio 
+    /// con multiples campos e interacciones con otras entidades mas simples
+    /// </summary>
+    public class NewEntity : BaseEntity
+    {
+        public string Nombre { get; set; } = null!;
+        public SimpleEntity Opcion { get; set; } = null!;
+    }
+}
---
+++ src/Empresa.Proyecto.Infra/Data/Configuration/NewEntityConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Empresa.Proyecto.Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Empresa.Proyecto.Infra.Data.Configuration
+{
+    internal class NewEntityConfig : IEntityTypeConfiguration<NewEntity>
+    {
+        public void Configure(EntityTypeBuilder<NewEntity> builder)
+        {
+            builder.Property(c => c.Nombre)
+                .HasMaxLength(50);
+        }
+    }
+}
